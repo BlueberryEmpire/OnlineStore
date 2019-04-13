f606b75827d497c4ad4d3cf07946057d6e3db7d3
cyanococcusrex <cyanococcusrex@blueberryempire.com>
2019-04-13 16:24:24 -0400

    Added new configuration settings.  Updated Style.css to set font on selects.  Updated relative paths on Header and Welcome page.

--- Source/Configuration.php
+++ Source/Configuration.php
@@ -2,17 +2,17 @@
 
 return array
 (
-	"AppDirectory" => "Store/Source",
+	"AppDirectory" => "BlueberryEmpire.com/Store/OnlineStore/Source",
 	"DatabaseServerName" => "localhost",
 	"DatabaseUsername" => "web",
 	"DatabasePassword" => "[redacted]",
 	"DatabaseName" => "Store",
+	"EmailAddressHelp" => "help@blueberryempire.com",
+	"EmailAddressNotify" => "notify@blueberryempire.com",
 	"EmailEnabled" => false,
-	"EmailAddressHelp" => "help@onlinestore.test",
-	"EmailAddressNotify" => "notify@onlinestore.test",
 	"ErrorReportingEnabled" => false,
-	"PaymentClientConfig" => "{ \"type\": \"Square\", \"accessToken\": \"[redacted]\", \"applicationID\": \"[redacted]\", \"locationID\": \"[redacted]\"}",
-	"SiteTitle" => "Online Store",
+	"PaymentClientConfig" => "{ \"type\": \"Square\", \"accessToken\": \"\[redacted]\", \"applicationID\": \"[redacted]\", \"locationID\": \"[redacted]\"}",
+	"SiteTitle" => "Blueberry Empire Store",
 );
 
 ?>
--- Source/Pages/Header.js
+++ Source/Pages/Header.js
@@ -2,7 +2,7 @@
 var d = document;
 var divHeader = d.createElement("div");
 divHeader.innerHTML =
-	"<a href='../../../index.html'><img src='../../../Images/BlueberryEmpire.svg'></img></a>"
+	"<a href='../../../../index.html'><img src='../../../../Images/BlueberryEmpire.svg'></img></a>"
 	+ "<br />"
 	+ "<h1>Store</h1>";
 d.body.appendChild(divHeader);
