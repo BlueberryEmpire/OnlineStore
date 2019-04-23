acecc46fc78446f5876a4774d5a85a25ec845eb6
cyanococcusrex <cyanococcusrex@blueberryempire.com>
2019-04-23 00:02:21 +0000

    Updated Data.sql to include product list.

--- Source/Database/Data.sql
+++ Source/Database/Data.sql
@@ -1,77 +1,68 @@
 use Store;
 
-/* ProductID, Name, ImagePath, Price, ContentPath, IsActive */
-insert into Product select 1, 'Product01', '../Images/Red.png', 	1.00, 'Product01', true;
-insert into Product select 2, 'Product02', '../Images/Orange.png', 	2.00, 'Product02', true;
-insert into Product select 3, 'Product03', '../Images/Yellow.png', 	3.00, 'Product03', true;
-insert into Product select 4, 'Product04', '../Images/Green.png', 	4.00, 'Product04', true;
-insert into Product select 5, 'Product05', '../Images/Blue.png', 	5.00, 'Product05', true;
-insert into Product select 6, 'Product06', '../Images/Violet.png', 	6.00, 'Product06', true;
-insert into Product select 7, 'Product07', '../Images/Black.png', 	7.00, '', true;
-insert into Product select 8, 'Product08', '../Images/Gray.png', 	0.00, '', true;
-insert into Product select 9, 'Product09', '../Images/White.png', 	8.00, '', true;
+set @imagePath = "../Images/Red.png";
+set @price = "5.00";
+set @contentPath = ".";
+set @isActive = 0;
 
-insert into Product select 10, 'Product10', '../Images/Red.png', 	1.00, '', true;
-insert into Product select 11, 'Product11', '../Images/Orange.png', 2.00, '', true;
-insert into Product select 12, 'Product12', '../Images/Yellow.png', 3.00, '', true;
-insert into Product select 13, 'Product13', '../Images/Green.png', 	4.00, '', true;
-insert into Product select 14, 'Product14', '../Images/Blue.png', 	5.00, '', true;
-insert into Product select 15, 'Product15', '../Images/Violet.png', 6.00, '', true;
-insert into Product select 16, 'Product16', '../Images/Black.png', 	7.00, '', true;
-insert into Product select 17, 'Product17', '../Images/Gray.png', 	0.00, '', true;
-insert into Product select 18, 'Product18', '../Images/White.png', 	8.00, '', true;
-insert into Product select 19, 'Product19', '../Images/Brown.png', 	9.00, '', true;
+delete from Product;
 
-insert into Product select 20, 'Product20', '../Images/Red.png', 	1.00, '', true;
-insert into Product select 21, 'Product21', '../Images/Orange.png', 2.00, '', true;
-insert into Product select 22, 'Product22', '../Images/Yellow.png', 3.00, '', true;
-insert into Product select 23, 'Product23', '../Images/Green.png', 	4.00, '', true;
-insert into Product select 24, 'Product24', '../Images/Blue.png', 	5.00, '', true;
-insert into Product select 25, 'Product25', '../Images/Violet.png', 6.00, '', true;
-insert into Product select 26, 'Product26', '../Images/Black.png', 	7.00, '', true;
-insert into Product select 27, 'Product27', '../Images/Gray.png', 	0.00, '', true;
-insert into Product select 28, 'Product28', '../Images/White.png', 	8.00, '', true;
-insert into Product select 29, 'Product29', '../Images/Brown.png', 	9.00, '', true;
+insert into Product
+select 0, 'Demo' '/Games/Demo/Title.png', 0.00, 'Demo', 1
+union all select 1, 'Adductor Wing', '/Games/AdductorWing/Title.png', @price, 
+'AdductorWing', @isActive
+union all select 2, 'Advanced Laser Pragmatics', 
+'/Games/AdvancedLaserPragmatics/Title.png', @price, 
+'AdvancedLaserPragmatics', @isActive
+union all select 3, 'Ascension Run', '/Games/AscensionRun/Title.png', 
+@price, 'AscensionRun', @isActive
+union all select 4, 'Bad Day on the Bathysphere', '/Games/BadDayOnTheBathysphere/Title.png', @price, 
+'BadDayOnTheBathysphere', @isActive
+union all select 5, 'Bing-Bang and the Brickbats', 
+'/Games/BingBangAndTheBrickbats/Title.png', @price, 
+'BingBangAndTheBrickbats', @isActive
+union all select 6, 'Blast Shadow', @imagePath, @price, 'BlastShadow', @isActive
+union all select 7, 'Chylde Norrick atop Bucreus Arydynge', 
+'/Games/ChyldeNorrickAtopBucreusArydynge/Title.png', 
+@price, 'ChyldeNorrickAtopBucreusArydynge', @isActive
+union all select 8, 'Dawnstar Station', 
+'/Games/DawnstarStation/Title.png', @price, 'DawnstarStation', @isActive
+union all select 9, 'Disputed Territory', @imagePath, @price, 'DisputedTerritory', @isActive
+union all select 10, 'Folddrive', @imagePath, @price, 'Folddrive', @isActive
+union all select 11, 'Freakybomb', '/Games/Freakybomb/Title.png', 
+@price, 'Freakybomb', @isActive
+union all select 12, 'Fretwork of Starfire', @imagePath, @price, 
+'FretworkOfStarfire', @isActive
+union all select 13, 'Fwaboom!', @imagePath, @price, 'Fwaboom', @isActive
+union all select 14, 'Gnome Farm', @imagePath, @price, 'GnomeFarm', @isActive
+union all select 15, 'Halls of Aeolus', 
+'/Games/HallsOfAeolus/Title.png', @price, 'HallsOfAeolus', @isActive
+union all select 16, 'Hell in Space', @imagePath, @price, 'HellInSpace', @isActive
+union all select 17, 'Iconoclast', '/Games/Iconoclast/Title.png', 
+@price, 'Iconoclast', @isActive
+union all select 18, 'Monster Machine', @imagePath, @price, 'MonsterMachine', @isActive
+union all select 19, 'Pirate or Ice Cream?', @imagePath, @price, 
+'PirateOrIceCream', @isActive
+union all select 20, 'Residuum Sublimae', @imagePath, @price, 
+'ResiduumSublimae', @isActive
+union all select 21, 'Shillelagh', @imagePath, @price, 'Shillelagh', @isActive
+union all select 22, 'Span of the Bow', @imagePath, @price, 'SpanOfTheBow', @isActive
+union all select 23, 'Starward Gyre', @imagePath, @price, 'StarwardGyre' , @isActive
+union all select 24, 'Tanglebox 41', @imagePath, @price, 'Tanglebox41', @isActive
+union all select 25, 'Thrones and Dominations', @imagePath, @price, 
+'ThronesAndDominations', @isActive
+union all select 26, 'Tranquillitatis', 
+'/Games/Tranquillitatis/Title.png', @price, 'Tranquillitatis', @isActive
+union all select 27, 'Transit of the Cosmotic', 
+'/Games/TransitOfTheCosmotic/Title.png', @price, 'TransitOfTheCosmotic', @isActive
+union all select 28, 'Unlit', '/Games/Unlit/Title.png', @price, 'Unlit', @isActive
+union all select 29, 'William the Thundrous', 
+'/Games/WilliamTheThundrous/Title.png', @price, 'WilliamTheThundrous', @isActive
+union all select 30, 'Wyvern''s Roost', @imagePath, @price, @contentPath, @isActive
+union all select 31, 'Ye Ettin''s Head', @imagePath, @price, 
+@contentPath, @isActive;
 
-insert into Product select 30, 'Product30', '../Images/Red.png', 	1.00, '', true;
-insert into Product select 31, 'Product31', '../Images/Orange.png', 2.00, '', true;
-insert into Product select 32, 'Product32', '../Images/Yellow.png', 3.00, '', true;
-insert into Product select 33, 'Product33', '../Images/Green.png', 	4.00, '', true;
-insert into Product select 34, 'Product34', '../Images/Blue.png', 	5.00, '', true;
-insert into Product select 35, 'Product35', '../Images/Violet.png', 6.00, '', true;
-insert into Product select 36, 'Product36', '../Images/Black.png', 	7.00, '', true;
-insert into Product select 37, 'Product37', '../Images/Gray.png', 	0.00, '', true;
-insert into Product select 38, 'Product38', '../Images/White.png', 	8.00, '', true;
-insert into Product select 39, 'Product39', '../Images/Brown.png', 	9.00, '', true;
 
-/* An inactive product test. */
-insert into Product select 99, 'Product99', '../Images/Gray.png', 	99.99, '', false;
 
-insert into Promotion (Description, Discount, Code) values ('Save $1 on Product1 when you buy Product2.', 1.00, '12341234-1234-1234-1234-123412341234');
-insert into Promotion_Product (PromotionID, ProductID) values (1, 1);
-insert into Promotion_Product (PromotionID, ProductID) values (1, 2);
 
-/* user "adam", password "Password_123" */
-insert into User (Username, EmailAddress, PasswordSalt, PasswordHashed, PasswordResetCode, IsActive) values ('adam', 'adam@localhost.localdomain', '1147748628', '874c1d861559fa124a3948a947bc1f6564ea478b56e37b976a9ad25bbd67092e', null, 1);
 
-insert into _Order (UserID, PromotionID, Status, TimeStarted, TimeUpdated, TimeCompleted, PaymentID) values (1, null, 'Completed', '2015-01-01', '2015-01-02', '2015-01-02', null);
-insert into Order_Product (OrderID, ProductID, Quantity) values (1, 2, 1);
-insert into Order_Product (OrderID, ProductID, Quantity) values (1, 3, 1);
-insert into Order_Product (OrderID, ProductID, Quantity) values (1, 4, 1);
-
-insert into License (UserID, ProductID) values (1, 1);
-insert into License (UserID, ProductID, TransferTypeID, TransferTarget) values (1, 2, 1, 'beth');
-insert into License (UserID, ProductID, TransferTypeID, TransferTarget) values (1, 3, 2, 'charlie@localhost.localdomain');
-insert into License (UserID, ProductID, TransferTypeID, TransferTarget) values (1, 4, 2, '12341234-1234-1234-1234-123412341234');
-
-/* user "beth", password "Password_123" */
-insert into User (Username, EmailAddress, PasswordSalt, PasswordHashed, PasswordResetCode, IsActive) values ('beth', 'beth@localhost.localdomain', '1147748628', '874c1d861559fa124a3948a947bc1f6564ea478b56e37b976a9ad25bbd67092e', null, 1);
-insert into License (UserID, ProductID) values (2, 1);
-
-/* user "charlie", password "Password_123" */
-insert into User (Username, EmailAddress, PasswordSalt, PasswordHashed, PasswordResetCode, IsActive) values ('charlie', 'charlie@localhost.localdomain', '1147748628', '874c1d861559fa124a3948a947bc1f6564ea478b56e37b976a9ad25bbd67092e', null, 1);
-insert into License (UserID, ProductID) values (3, 1);
-
-/* user "diane", password "Password_123" */
-insert into User (Username, EmailAddress, PasswordSalt, PasswordHashed, PasswordResetCode, IsActive) values ('diane', 'diane@localhost.localdomain', '1147748628', '874c1d861559fa124a3948a947bc1f6564ea478b56e37b976a9ad25bbd67092e', null, 1);
-insert into License (UserID, ProductID) values (3, 1);
