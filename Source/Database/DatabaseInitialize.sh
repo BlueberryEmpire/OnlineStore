21e066b59f819107077e83fef75b9fb28e82180a
cyanococcusrex <cyanococcusrex@blueberryempire.com>
2019-04-25 18:21:46 -0400

    Updated database scripts to better handle creation of unprivileged 'web' user.

--- Source/Database/DatabaseInitialize.sh
+++ Source/Database/DatabaseInitialize.sh
@@ -2,16 +2,26 @@
 
 echo "Script begins."
 
-read -p "Enter the host name: " hostName
-read -p "Enter the database username: " username
-read -s -p "Enter the database password: " password
+read -p "Host name: " hostName
+read -p "Database admin username: " adminUsername
+read -s -p "Database admin password: " adminPassword
+echo ""
+read -s -p "Unprivileged user password: " webPassword
+echo ""
+
+echo "Creating unprivileged user..."
+echo "drop user if exists 'web'@'localhost';" > CreateWebUser.sql
+echo "create user if not exists 'web'@'localhost' identified by '"$webPassword"';" >> CreateWebUser.sql
+mysql -h $hostName -u $adminUsername --password=$adminPassword < CreateWebUser.sql
+rm CreateWebUser.sql
+echo "...done creating unprivileged user."
 
 echo "Running Schema.sql..."
-mysql -h $hostName -u $username --password=$password < Schema.sql
+mysql -h $hostName -u $adminUsername --password=$adminPassword < Schema.sql
 echo "...done running Schema.sql."
 
 echo "Running Data.sql..."
-mysql -h $hostName -u $username --password=$password < Data.sql
+mysql -h $hostName -u $adminUsername --password=$adminPassword < Data.sql
 echo "...done running Data.sql."
 
 echo "Script ends."
